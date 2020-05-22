424625c1ab1af08c2d906c024f6b07b6f6f7ae14
Anant Jindal <anantjindal1@gmail.com>
2020-05-22 10:26:47 +0530

    code - year divisible by 400 should be leap

--- src/main/LeapYear.java
+++ src/main/LeapYear.java
@@ -3,4 +3,13 @@ public class LeapYear {
   public LeapYear(){
 
   }
+
+  public boolean isLeap(int year) {
+		boolean result = isMultipleOf(year,400);
+		return result;
+	}
+
+  private boolean isMultipleOf(int num, int base) {
+		return ((num % base) == 0);
+	}
 }
