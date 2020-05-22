7132e90aa816358d71e51bf53a72304d6c9ca65a
Anant Jindal <anantjindal1@gmail.com>
2020-05-22 10:30:19 +0530

    tests - for testing Leap year

--- src/test/TestLeapYear.java
+++ src/test/TestLeapYear.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 import org.junit.Before;
+import static org.junit.Assert.*;
+
 
 public class TestLeapYear {
 
@@ -15,6 +17,29 @@ public class TestLeapYear {
 		public void yr2000ShouldBeLeap() {
 			boolean isLeap = leapYear.isLeap(2000);
 			assertTrue(isLeap);
+		}
+
+		//All years divisible by 100 but not by 400 are NOT leap years (so, for example, 1700, 1800, and 1900 were NOT leap years, NOR will 2100 be a leap year),
+		@Test
+		public void yr1800ShouldNotBeLeap() {
+			boolean isLeap = leapYear.isLeap(1800);
+			assertFalse(isLeap);
+		}
+
+
+		//All years divisible by 4 but not by 100 ARE leap years (e.g., 2008, 2012, 2016),
+		@Test
+		public void yr2008ShouldBeLeap() {
+			boolean isLeap = leapYear.isLeap(2008);
+			assertTrue(isLeap);
+		}
+
+
+		//All years not divisible by 4 are NOT leap years (e.g. 2017, 2018, 2019).
+		@Test
+		public void yr2017ShouldNotBeLeap() {
+			boolean isLeap = leapYear.isLeap(2017);
+			assertFalse(isLeap);
 		}
 
 
