3945a0d5f521bb625c3a8889bbb05115bf2dd2e0
Anant Jindal <anantjindal1@gmail.com>
2020-05-22 10:19:22 +0530

    test- year divisible by 400 should be a leap

--- src/test/TestLeapYear.java
+++ src/test/TestLeapYear.java
@@ -1,12 +1,21 @@
 import org.junit.Test;
+import org.junit.Before;
 
 public class TestLeapYear {
 
-	private LeapYear leapYear;
+		private LeapYear leapYear;
 
-	@Test
-	public void setUp() throws Exception {
-		leapYear = new LeapYear();
-	}
+		@Before
+		public void setUp() throws Exception {
+			leapYear = new LeapYear();
+		}
+
+		//All years divisible by 400 ARE leap years (so, for example, 2000 was indeed a leap year),
+		@Test
+		public void yr2000ShouldBeLeap() {
+			boolean isLeap = leapYear.isLeap(2000);
+			assertTrue(isLeap);
+		}
+
 
 }
